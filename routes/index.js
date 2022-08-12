21cf1af0b736ea8d5e2abe6c61bdfc18984b6e00
Steve C <steve@example.com>
2022-08-11 18:48:19 -0700

    Add about page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Git Demo' });
 });
 
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Us' });
+});
+
 module.exports = router;
